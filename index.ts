6ad10631faa4969b8732f9ab4f461b4ce974421d
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-08 06:19:27 +0300

    fin

--- index.ts
+++ index.ts
@@ -1,11 +1,18 @@
 import { merge } from 'rxjs/operators';
 import { interval } from 'rxjs';
 
-//emit every 2.5 seconds
-const first = interval(4500);
-//emit every 1 second
-const second = interval(3000);
+// https://www.learnrxjs.io/learn-rxjs/operators/combination/merge
+// Example 2: merge 2 observables, instance method
+
+//emit every 5 seconds
+// const first = interval(4500);
+// const second = interval(3000);
+// const example = first.pipe(merge(second));
+// const subscribe = example.subscribe((val) => console.log(val));
 //used as instance method
-const example = first.pipe(merge(second));
 //output: 0,1,0,2....
-const subscribe = example.subscribe((val) => console.log(val));
+
+const observ_1 = interval(10500);
+const observ_2 = interval(9000);
+const exmp = observ_1.pipe(merge(observ_2));
+const sub = exmp.subscribe((val) => console.log(val));
